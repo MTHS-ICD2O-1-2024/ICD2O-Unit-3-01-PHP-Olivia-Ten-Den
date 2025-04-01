d9f91c16bba3ec63699c8357decc218d3937e8e9
default <olivia.ten-den@stu.ocsb.ca>
2025-04-01 16:56:08 +0000

    index chanded names

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<!-- ICS2O-Unit2-04-HTML-PHP -->
+<!-- ICS2O-Unit3-01-HTML-PHP -->
 <html lang="en-ca">
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="The Area of Triangle program, PHP" />
+  <meta name="description" content="The Area of Trapezoid, PHP" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Olivia TD" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -30,7 +30,7 @@
     sizes="16x16"
     href="./favicon-16x16.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>The Area of Triangle program, PHP</title>
+  <title>The Area of Trapezoid, PHP</title>
 </head>
 
 <body>
@@ -38,28 +38,40 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">The Area of Triangle program, PHP</span>
+        <span class="mdl-layout-title">The Area of Trapezoid, PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/area_of_triangle.png" alt="White sign that shows the formula to calculate the area of a triangle, image" width="250" />
+        <img src="./images/trapezoid.svg" alt="Image of trapezoid with labeled sides.:" width="250" />
       </div>
       <br />
-      <div class="page-content">Enter Triangle's base and height in cm.</div>
+      <div class="page-content"> Please enter integers for dimentions (mm).</div>
       <div class="page-content-php">
         <form action="answer.php" method="GET">
+          a Base:
           <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="base-of-triangle">
-            <label class="mdl-textfield__label" for="base-of-triangle">Enter base of triangle here</label>
+            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="a-base-of-trapezoid">
+            <label class="mdl-textfield__label" for="a-base-of-trapezoid">Enter a Base of trapezoid here (mm)</label>
             <span class="mdl-textfield__error">Input is not a number!</span>
           </div>
+          mm
           <br />
+          b Base:
           <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="height-of-triangle">
-            <label class="mdl-textfield__label" for="height-of-triangle">Enter height of triangle here</label>
+            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="b-base-of-trapezoid">
+            <label class="mdl-textfield__label" for="b-base-of-trapezoid">Enter b base of trapezoid here (mm)</label>
             <span class="mdl-textfield__error">Input is not a number!</span>
           </div>
+          mm
+          <br />
+          Height:
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="height-of-trapezoid">
+            <label class="mdl-textfield__label" for="height-of-trapezoid">Enter height of trapezoid here (mm)</label>
+            <span class="mdl-textfield__error">Input is not a number!</span>
+          </div>
+          mm
           <br />
           <!-- Accent-colored raised button with ripple -->
           <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
